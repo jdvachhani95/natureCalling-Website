7d216ab6d6f91ca44e32a5eb78761b4b60693876
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-02 12:45:12 -0400

    start local server at 3000 port

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("initial java script file");
+var express = require("express");
+var app = new express();
+
+app.get("/", function(req, res) {
+    res.send("this will become our landing page soon");
+});
+
+app.get("*",function(req,res){
+    res.send("Send something else.");
+});
+
+var port = 3000;
+app.listen(port, function() {
+    console.log("Server Connected at "+port);
+});
+
