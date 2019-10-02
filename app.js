b88229b70085ab25f2d972e9218d3cef63d28852
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-02 13:26:45 -0400

    initial setup of home page and campgroung page

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var app = new express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-    res.send("this will become our landing page soon");
+    res.render("home");
+});
+
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name:"Salman creek", image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name:"Granband", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name:"Port stenly", image:"https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name:"Tobermory", image:"https://images.unsplash.com/photo-1545153996-e01b50d6ec38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name:"Gas pe", image:"https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+    ]
+    res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.get("*",function(req,res){
