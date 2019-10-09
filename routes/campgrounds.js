d9bc5c8d50281a165ece549914b76ea75ca082ff
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-09 13:38:43 -0400

    add update campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -62,6 +62,18 @@ router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
     });
 });
 
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id",middleware.checkCampgroundOwnership, function(req, res){
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
 
 //Midleware
 function isLoggedIn(req, res, next){
