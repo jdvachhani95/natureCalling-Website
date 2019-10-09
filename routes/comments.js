cf77f0a0bdc7a2c36c1290b4c6ef031fcd52ea70
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-09 14:14:40 -0400

    add comment edit/delete function

--- routes/comments.js
+++ routes/comments.js
@@ -42,6 +42,40 @@ router.post("/", function(req, res){
    });
 });
 
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+       if(err){
+           res.redirect("back");
+       } else {
+         res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+       }
+    });
+ });
+ 
+ // COMMENT UPDATE
+ router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id );
+       }
+    });
+ });
+ 
+ // COMMENT DESTROY ROUTE
+ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+     //findByIdAndRemove
+     Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+     });
+ });
+
 //Midleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
