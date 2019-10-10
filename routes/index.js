f1d972a63ad0d28a4cdb2b6d41ae7d7993107ed7
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-09 22:46:46 -0400

    delete commented part

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,10 @@ router.get("/", function(req, res) {
     res.render("landing");
 });
 
-
-
 // show register form
 router.get("/register", function(req, res){
     res.render("register", {page: 'register'}); 
  });
-
-//  //handle sign up logic
-//  router.post("/register", function(req, res){
-//      var newUser = new User({username: req.body.username});
-//      User.register(newUser, req.body.password, function(err, user){
-//          if(err){
-//             req.flash("error", err.message);
-//             return res.render("register");
-//          }
-//          passport.authenticate("local")(req, res, function(){
-//             req.flash("success", "Welcome to YelpCamp " + user.username);
-//             res.redirect("/campgrounds"); 
-//          });
-//      });
-//  });
 
  //handle sign up logic
 router.post("/register", function(req, res){
